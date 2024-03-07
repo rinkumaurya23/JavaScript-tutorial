d1e5be47b0409adcb51b43cba64f22ab322742c5
Rinku Maurya <126308215+rinkumaurya23@users.noreply.github.com>
2024-03-07 18:39:23 +0000

    datatype

---
+++ 01_basics/02_dataType.js
@@ -0,0 +1,28 @@
+"use strict";
+  // treat all JS code as newer version
+
+
+//   alert("hello") // we are using nodejs,not browser
+
+// ? Code  readability should  be high
+// console.log(3+3);
+// console.log("Rinku")
+
+
+let name = "Rinku";
+let age = 22;
+let isLoggedIn = false;
+
+
+// Number => 2 to power 53
+// bigInt => 
+//  String => '',""
+//  Boolean => true,false
+// Null =>standalone value  (empty) Ex:- tempratue 0 = null
+// Undefine => Value not define  
+// Symbol => Unique
+
+// Object
+
+// console.log(typeof null); // object
+
