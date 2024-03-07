48afd1c93de030a9a04893f3512f6e2b2115b15b
Rinku Maurya <126308215+rinkumaurya23@users.noreply.github.com>
2024-03-07 18:21:30 +0000

    variable declarele

---
+++ 01_basics/o1_variable.js
@@ -0,0 +1,33 @@
+let  account_Id = 14453;// this is constant variable // compiler v8 engine to a compile the code \\* do not change the value*
+let accountEmail = "rinkubhai2143200@gmail.com";
+var accountPassword = "1234";
+accountCity = "Jaipur";
+let accountState;   //  undefine this is not define the value 
+
+// account_Id= 2
+// console.log(account_Id);
+
+accountEmail = "maurya@gmail.com"
+accountPassword="12321234321"
+accountCity = "Gonda"
+  
+
+// {
+    // this is scope 
+
+    // for (let index = 0; index < array.length; index++) {
+    //     const element = array[index];
+        
+    // }
+// }
+
+
+console.log(account_Id);
+
+/*
+Prefer to not use var 
+because of issue in block scope and function 
+ */
+
+
+console.table([account_Id, accountEmail,accountState, accountPassword,accountCity])
