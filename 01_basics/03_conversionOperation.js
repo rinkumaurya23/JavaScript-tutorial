85d18e4721be3fbc0500b2954b38604c7ce1b9fa
Rinku Maurya <126308215+rinkumaurya23@users.noreply.github.com>
2024-03-07 18:57:43 +0000

    conversion of datatype

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,30 @@
+// let score  =    "Rinku"
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let  valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+//  Basic conversion
+//  "33" =>33
+// "abc" => NaN
+// true => 1
+
+// let isLoggedIn = ""
+// let booleanIsLogged = Boolean(isLoggedIn)
+// console.log(booleanIsLogged);
+
+
+// 1 => true 
+// 0 => false
+// "" => false
+// "Rinku" => true
+
+
+let somenumber = 32
+
+let stringNumber = String(somenumber);
+
+console.log(stringNumber)  //33
+console.log(typeof stringNumber)  // string
+
