c20b91197b8f3857a24da311766c20362e37a3a5
Rinku Maurya <126308215+rinkumaurya23@users.noreply.github.com>
2024-03-11 17:32:28 +0000

    cg

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,30 +1,73 @@
-// let score  =    "Rinku"
-// console.log(typeof score);
-// console.log(typeof(score));
+// // let score  =    "Rinku"
+// // console.log(typeof score);
+// // console.log(typeof(score));
 
-// let  valueInNumber = Number(score);
-// console.log(typeof valueInNumber);
-// console.log(valueInNumber);
-//  Basic conversion
-//  "33" =>33
-// "abc" => NaN
-// true => 1
+// // let  valueInNumber = Number(score);
+// // console.log(typeof valueInNumber);
+// // console.log(valueInNumber);
+// //  Basic conversion
+// //  "33" =>33
+// // "abc" => NaN
+// // true => 1
 
-// let isLoggedIn = ""
-// let booleanIsLogged = Boolean(isLoggedIn)
-// console.log(booleanIsLogged);
-
-
-// 1 => true 
-// 0 => false
-// "" => false
-// "Rinku" => true
+// // let isLoggedIn = ""
+// // let booleanIsLogged = Boolean(isLoggedIn)
+// // console.log(booleanIsLogged);
 
 
-let somenumber = 32
+// // 1 => true 
+// // 0 => false
+// // "" => false
+// // "Rinku" => true
 
-let stringNumber = String(somenumber);
 
-console.log(stringNumber)  //33
-console.log(typeof stringNumber)  // string
+// let somenumber = 32
+
+// let stringNumber = String(somenumber);
+
+// console.log(stringNumber)  //33
+// console.log(typeof stringNumber)  // string
+
+// 88888888888888888888888 Operation 8888888888888888888888888888
+
+
+let value = 3
+let negValue = -value
+// console.log(negValue)  //-3
+
+// console.log(2+3)
+// console.log(2-3)
+// console.log(2/3)
+// console.log(2%3)
+// console.log(2**3)
+// console.log(2*3)
+// console.log(2==3)
+// 5
+// -1
+// 0.6666666666666666
+// 2
+// 8
+// 6
+// false
+
+let fname = "Rinku"
+let lname = "Maurya"
+
+
+let str = fname+lname
+console.log(str);
+
+
+
+
+// console.log("1" + 2);  
+// console.log("1" +2+3);
+// console.log(1 + "3");
+// 12
+// 123
+// 13
+
+
+
+
 
